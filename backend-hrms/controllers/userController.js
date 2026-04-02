d90c9990f994dd3ed7b443706d2aacd9267a1b5a
shyam-v-equest <shyam.vachhani-i@equestsolutions.net>
2026-04-02 18:02:58 +0530

    User page fixed once again!

--- backend-hrms/controllers/userController.js
+++ backend-hrms/controllers/userController.js
@@ -1,5 +1,5 @@
 import db from "../config/db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 export const addUser = async (req, res) => {
   const { username, email, password, role } = req.body;
 
